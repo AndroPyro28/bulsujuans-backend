a4ac457d5f5caa61b65556fcb346e1fc1a15b8b3
JhondelCaranay <jhondeldelconacaranay@gmail.com>
2025-10-07 14:26:17 +0800

    add new permission middlewares and fix type on seeder

--- src/app/roles/routes.ts
+++ src/app/roles/routes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import RoleController from "./controllers";
-import { permission } from "../../middlewares/permission";
+import { hasAllPermission } from "../../middlewares/permission";
 
 const router: Router = Router();
 
 const roleController = new RoleController();
 
-router.get("/list", permission("users:view_list"), roleController.list);
-router.get("/show/:id", permission("users:view_detail"), roleController.show);
-router.post("/store", permission("users:create"), roleController.store);
-router.patch("update/:id", permission("users:edit"), roleController.update);
-router.delete("/destroy/:id", permission("users:delete"), roleController.destroy);
+router.get("/list", hasAllPermission(["users:view_list"]), roleController.list);
+router.get("/show/:id", hasAllPermission(["users:view_detail"]), roleController.show);
+router.post("/store", hasAllPermission(["users:create"]), roleController.store);
+router.patch("update/:id", hasAllPermission(["users:edit"]), roleController.update);
+router.delete("/destroy/:id", hasAllPermission(["users:delete"]), roleController.destroy);
 
 export default router;
