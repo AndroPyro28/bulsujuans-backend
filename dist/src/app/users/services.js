9d0b81da27ce6f092bbacc09fe08ab9d1ab8c5ef
AndroPyro28 <menandroeugenio1028@gmail.com>
2025-11-04 17:54:11 +0800

    fix: tsconfig

--- dist/src/app/users/services.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class UserService {
-    constructor() { }
-    async create(body) {
-        return body;
-    }
-}
-exports.default = UserService;
--- dist/src/lib/prisma.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseEnumParam = parseEnumParam;
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
-exports.default = prisma;
-function parseEnumParam(value, enumObj) {
-    return Object.values(enumObj).includes(value) ? value : undefined;
-}
